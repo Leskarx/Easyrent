7d7e85c7a6fef2930d251b5592031961ebc83bb6
Leskarx <leskarzone123@gmail.com>
2024-04-14 11:00:01 +0530

    sign up form

--- src/components/SignupComponents/Signup.jsx
+++ src/components/SignupComponents/Signup.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import AlreadySection from '../utils/AlreadySection'
 import InputBox from '../utils/InputBox'
 import MainSection from '../utils/MainSection'
+import { useForm } from "react-hook-form"
 
 export default function Signup() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } =useForm({
+    email:"",
+    username:"",
+    number:"",
+    password:"",
+
+  })
+  function onSubmit(data){
+    console.log(data)
+  }
     const footer=(
         <>
         
@@ -15,10 +31,10 @@ export default function Signup() {
       const body=(
         <>
         <div>
-        <InputBox title="Email" type="text" name="email" />
-        <InputBox title="Name" type="text" name="username" />
-        <InputBox title="Phone Number" type="text" name="mobile" />
-        <InputBox title="Password" type="password" />
+        <InputBox register={register} errors={errors} objectId="email" placeholder="Email" type="text" name="email" />
+        <InputBox register={register} errors={errors} objectId="username" placeholder="Name" type="text" name="username" />
+        <InputBox register={register} errors={errors} objectId="number" placeholder="Phone number" type="text" name="number" />
+        <InputBox register={register} errors={errors} objectId="password" placeholder="Password" type="password" name="password" />
         </div>
         
         </>
@@ -26,7 +42,7 @@ export default function Signup() {
     
       return (
         <div className=' w-full h-full flex justify-center items-center md:mb-10 mb-0 '>
-      <MainSection title="Register" para='Create an account to access all the features of EASYRENT!' buttonTitle="Register" footer={footer} body={body} showGprovider={false} />
+      <MainSection title="Register" para='Create an account to access all the features of EASYRENT!' handleSubmit={handleSubmit(onSubmit)} buttonTitle="Register" footer={footer} body={body} showGprovider={false} />
         </div>
       )
 }
