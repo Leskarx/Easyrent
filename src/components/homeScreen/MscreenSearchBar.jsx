18fd5bf2fd727d768e54c64b4d421168669ec051
Leskarx <leskarzone123@gmail.com>
2024-06-16 21:26:30 +0530

    Refactor MscreenSearchBar component and add mobile search bar

--- src/components/homeScreen/MscreenSearchBar.jsx
+++ src/components/homeScreen/MscreenSearchBar.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import MscreenText from '../utils/text/MscreenText'
+import Button from '../utils/Button/Button'
 
 export default function MscreenSearchBar() {
   return (
-    <main className=' absolute top-24 left-1/2 rounded-xl shadow-black/50 drop-shadow-2xl shadow-2xl -translate-x-1/2 bg-white p-4 h-[100px] w-[75%]'>
-        <section className=' w-full h-full'>
+    <main className=' absolute hidden md:block top-24 left-1/2 rounded-xl shadow-black/50 drop-shadow-2xl shadow-2xl -translate-x-1/2 bg-white p-4  w-max'>
+       <div className=' w-full h-full flex flex-col gap-2'>
+      {/* text section */}
+      <main className=' w-full h-max'>
+      <p className=' font-semibold text-xl'>
+        Studying outside your Home-Town?
+        </p>
+        <p className=' font-light text-sm'>
+        Search your perfect home in seconds
+        </p>
+      </main>
+       
+       <section className=' w-full h-full flex flex-row items-center gap-14'>
+<MscreenText/>
+<MscreenText/>
+<div className=' w-[30%] h-full flex justify-center items-center'>
+<Button buttonTitle={"Search"} />
+</div>
+
 
         </section>
+       </div>
       
     </main>
   )
---
+++ src/components/utils/text/MscreenText.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MscreenText() {
+  return (
+    <div className=' w-[14rem] h-[4.4rem] border-2'>
+      
+    </div>
+  )
+}
